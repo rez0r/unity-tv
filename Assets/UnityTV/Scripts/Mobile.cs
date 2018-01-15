7e4a3ef983f7ef7bfa69293d0e97b8c6b42b591e
David Baron <info@nerdtron.com>
2018-01-14 19:47:58 -0500

    Adding basic implementations to start supporting mobile platforms

---
+++ Assets/UnityTV/Scripts/Mobile.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Mobile : MonoBehaviour
+{
+	void Start ()
+    {
+        Handheld.PlayFullScreenMovie("https://ia802503.us.archive.org/10/items/1977GentlemenPreferHanesPantyhose/1977%20Gentlemen%20Prefer%20Hanes%20Pantyhose.ogv", Color.black, FullScreenMovieControlMode.CancelOnInput);
+    }
+}
